687da786a7bfe1d1574d5c798b706118bd4a2eff
Tolga077 <105724628+Tolga077@users.noreply.github.com>
2022-06-27 17:44:30 +0300

    Update build_revanced.sh

--- build_revanced.sh
+++ build_revanced.sh
@@ -71,6 +71,7 @@ then
                                -a com.google.android.youtube.apk -o build/revanced-root.apk
     echo "Building Non-root APK"
     java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar  \ 
+                               -e custom-branding \
                                -a com.google.android.youtube.apk -o build/revanced-nonroot.apk
 else
     echo "Cannot find YouTube APK, skipping build"
@@ -83,10 +84,11 @@ if [ -f "com.google.android.apps.youtube.music.apk" ]
 then
     echo "Building Root APK"
     java -jar revanced-cli.jar -b revanced-patches.jar --mount \
-                               -e microg-support \
+                               -e microg-support -e custom-branding \
                                -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
     echo "Building Non-root APK"
     java -jar revanced-cli.jar -b revanced-patches.jar --mount \ 
+                               -e custom-branding \
                                -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
 else
     echo "Cannot find YouTube Music APK, skipping build"
